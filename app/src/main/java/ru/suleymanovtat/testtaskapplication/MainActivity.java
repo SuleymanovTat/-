d74f98758e8779d1f84bb9ea934d4c2a46aa3347
suleymanovtat <suleymanovtat@gmail.com>
2018-02-22 23:54:04 +0300

    Created a project with an empty Activity. In the layout added EditText and Button. Did by clicking on the button, we display Toast with the text entered. If there is no text in EditText, nothing happens by clicking on the Button.

---
+++ app/src/main/java/ru/suleymanovtat/testtaskapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package ru.suleymanovtat.testtaskapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edText = findViewById(R.id.edText);
+        Button btnShowText = findViewById(R.id.btnShowText);
+        btnShowText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (isTextValid()) {
+            showMessage(edText.getText().toString());
+        }
+    }
+
+    private boolean isTextValid() {
+        return !TextUtils.isEmpty(edText.getText().toString().trim());
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+}
